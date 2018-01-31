d33bc4e19ac16f199ecce56546e5e240be4d66dc
Harish <hthuwal@gmail.com>
2018-01-31 14:10:08 +0530

    File Rename and some todos

--- utility.py
+++ utility.py
@@ -16,9 +16,10 @@ def signal_strength_to_distance(signal, freq):
 		freq {number} -- [frequency of the signal] (default: {2.4})
 	"""
 	distance = 10 ** ((27.55 + abs(signal) - (20 * math.log10(freq*1000)))/20.0)
+	#TODO convert to feets
 	return distance
 
-def intersection_of_three_circels(x1, y1, r1, x2, y2, r2, x3, y3, r3):
+def intersection_of_three_circel(x1, y1, r1, x2, y2, r2, x3, y3, r3):
 	
 	# Todo handle edge cases if any
 	c1 = Circle(Point(x1,y1), r1)
