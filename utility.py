f5dad8762d87333580a53713975cd7e82d4d2084
Harish <hthuwal@gmail.com>
2018-01-31 13:36:22 +0530

    Preiliminary intersection function

--- utility.py
+++ utility.py
@@ -1,4 +1,8 @@
 import math
+from sympy import Point, Circle, intersection
+import matplotlib.pyplot as plt
+
+
 
 def signal_strength_to_distance(signal, freq):
 	"""
@@ -13,3 +17,16 @@ def signal_strength_to_distance(signal, freq):
 	"""
 	distance = 10 ** ((27.55 + abs(signal) - (20 * math.log10(freq*1000)))/20.0)
 	return distance
+
+def intersection_of_three_circels(x1, y1, r1, x2, y2, r2, x3, y3, r3):
+	
+	# Todo handle edge cases if any
+	c1 = Circle(Point(x1,y1), r1)
+	c2 = Circle(Point(x2,y2), r2)
+	c3 = Circle(Point(x3,y3), r3)
+	ans = intersection(c1,c2,c3)
+	if len(ans)!=0:
+		ans = [(p.x, p.y) for p in ans]
+		return ans
+	else:
+		return None
