8a46efbd5c6e62a03f641af29b18bc0c70e38594
Harish <hthuwal@gmail.com>
2018-01-31 12:18:02 +0530

    pep8

--- test.py
+++ test.py
@@ -6,21 +6,22 @@ f = os.listdir('logs')
 f = [j for j in f if '2018' in j]
 
 for file in f:
-	df_day = pd.read_json('logs/'+file+'/0.log', lines=True)
-	df_day['ts'] = pd.to_datetime(df_day['ts'], infer_datetime_format=True)
-	for num in range(1, 24):
-		df_hour = pd.read_json('logs/'+file+'/'+str(num)+'.log', lines=True)
-		df_hour['ts'] = pd.to_datetime(df_hour['ts'], infer_datetime_format=True)
-		df_day = df_day.append(df_hour, ignore_index=True)
+    df_day = pd.read_json('logs/'+file+'/0.log', lines=True)
+    df_day['ts'] = pd.to_datetime(df_day['ts'], infer_datetime_format=True)
+    for num in range(1, 24):
+        df_hour = pd.read_json('logs/'+file+'/'+str(num)+'.log', lines=True)
+        df_hour['ts'] = pd.to_datetime(
+            df_hour['ts'], infer_datetime_format=True)
+        df_day = df_day.append(df_hour, ignore_index=True)
 
-	df_day.reset_index(inplace=True)
-	del df_day['index']	
+    df_day.reset_index(inplace=True)
+    del df_day['index']
 
-	df1 = df_day.groupby('mac')
-	df1 = df1.count()
-	df1.sort_values('nasid', inplace=True, ascending=False)
-	mac = df1.index[0]
-	count_probes = df1.loc[mac, 'nasid']
-	total_probes = len(df_day)
+    df1 = df_day.groupby('mac')
+    df1 = df1.count()
+    df1.sort_values('nasid', inplace=True, ascending=False)
+    mac = df1.index[0]
+    count_probes = df1.loc[mac, 'nasid']
+    total_probes = len(df_day)
 
-	print file + ', mac - ' + str(mac) + ', count_probes - ' + str(count_probes) + ', total_probes - ' + str(total_probes) + ', %tage - ' + str(count_probes*100/total_probes)
+    print file + ', mac - ' + str(mac) + ', count_probes - ' + str(count_probes) + ', total_probes - ' + str(total_probes) + ', %tage - ' + str(count_probes*100/total_probes)
