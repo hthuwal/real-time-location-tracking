9272b5cf8e0738dad3bbf542a090c6147be510e4
Harish <hthuwal@gmail.com>
2018-02-03 00:45:13 +0530

    Intersection function now accepts three triplets instead of 9 independent paramters

--- utility.py
+++ utility.py
@@ -19,12 +19,13 @@ def signal_strength_to_distance(signal, freq):
 	distance = distance * 3.28084 #converting into foot
 	return distance
 
-def intersection_of_three_circel(x1, y1, r1, x2, y2, r2, x3, y3, r3):
+def intersection_of_three_circles(c1, c2, c3):
 	
 	# Todo handle edge cases if any
-	c1 = Circle(Point(x1,y1), r1)
-	c2 = Circle(Point(x2,y2), r2)
-	c3 = Circle(Point(x3,y3), r3)
+	print c1, c2, c3
+	c1 = Circle(Point(c1[0],c1[1]), c1[2])
+	c2 = Circle(Point(c2[0],c2[1]), c2[2])
+	c3 = Circle(Point(c3[0],c3[1]), c3[2])
 	ans = intersection(c1,c2,c3)
 	if len(ans)!=0:
 		ans = [(p.x, p.y) for p in ans]
