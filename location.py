8d1abcdf1240f5fcf869968a2d45d2e6e2a6a295
Harish Thuwal <hthuwal@gmail.com>
2018-02-14 15:01:12 +0530

    Coordinates of APs and function to plot circles

--- location.py
+++ location.py
@@ -1,10 +1,30 @@
 from __future__ import division
+import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
 
 macs = ["74:23:44:33:2f:b7", "00:0c:e7:4f:38:a5", "88:36:5f:f8:3b:4a", "84:38:38:f6:58:40", "c0:ee:fb:72:0c:27", "18:dc:56:8c:27:56", "80:58:f8:d8:ad:e1"]
 
-df = pd.read_json("spencers_data/10.log", lines=True)  # reading data
+aps = {
+    1: (-22, 1),
+    2: (0, 1),
+    3: (0, 24),
+    4: (-22, 26)
+}
+
+
+def plot_circles(circles):
+    """
+        Function to plot the initial decision boundary plot
+    """
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title('boundaries')
+
+    for circle in circles:
+        c = plt.Circle(circle[0], circle[1], color='b', fill=False)
+
+
+df = pd.read_json("spencers_data/11.log", lines=True)  # reading data
 df['ts'] = pd.to_datetime(df['ts'], infer_datetime_format=True)
 
 
