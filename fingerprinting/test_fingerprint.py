885dff953baaa5d8a68b5f74e138334a60b54d64
Harish Thuwal <hthuwal@gmail.com>
2018-03-20 21:54:04 +0530

    Pep8

--- fingerprinting/test_fingerprint.py
+++ fingerprinting/test_fingerprint.py
@@ -14,15 +14,15 @@ df_fingerprint = pd.read_csv('gen_csvs/fingerprint.csv')
 df_device_new = pd.DataFrame(columns=['ts', 'controllerid', 'pwr', 'pred_loc', 'act_loc'])
 
 for i in range(len(df_device)):
-	# print df_device.loc[i, 'controllerid']
-	df_device.loc[i, 'controllerid'] = df_device.loc[i, 'controllerid'][1:-1]
-	lst = df_device.loc[i, 'controllerid'].split(', ')
-	df_device_new.loc[i, 'controllerid'] = lst
-	df_device_new.loc[i, 'pwr'] = df_device.loc[i, 'pwr'][1:-1].split(', ')
-	df_device_new.loc[i, 'ts'] = df_device.loc[i, 'ts']
+    # print df_device.loc[i, 'controllerid']
+    df_device.loc[i, 'controllerid'] = df_device.loc[i, 'controllerid'][1:-1]
+    lst = df_device.loc[i, 'controllerid'].split(', ')
+    df_device_new.loc[i, 'controllerid'] = lst
+    df_device_new.loc[i, 'pwr'] = df_device.loc[i, 'pwr'][1:-1].split(', ')
+    df_device_new.loc[i, 'ts'] = df_device.loc[i, 'ts']
 
-	df_device_new.loc[i, 'controllerid'] = map(int, df_device_new.loc[i, 'controllerid'])
-	df_device_new.loc[i, 'pwr'] = map(float, df_device_new.loc[i, 'pwr'])
+    df_device_new.loc[i, 'controllerid'] = list(map(int, df_device_new.loc[i, 'controllerid']))
+    df_device_new.loc[i, 'pwr'] = list(map(float, df_device_new.loc[i, 'pwr']))
 
 df_device_new['ts'] = pd.to_datetime(df_device_new['ts'], infer_datetime_format=True)
 df_device_new['ts'] = df_device_new['ts'].apply(lambda x: x + datetime.timedelta(hours=5, minutes=30))  # converrting utc time ist
@@ -44,7 +44,7 @@ for i in range(len(df_device_new)):
     x = df_fingerprint.loc[idx, 'X']
     y = df_fingerprint.loc[idx, 'Y']
 
-	df_device_new.loc[i, 'pred_loc'] = [x, y]
+    df_device_new.loc[i, 'pred_loc'] = [x, y]
 
 # print df_device_new
 
@@ -52,16 +52,15 @@ df_path1 = pd.read_csv('../spencers_data/path1.csv')
 df_path1['Start_time'] = pd.to_datetime(df_path1['Start_time'], infer_datetime_format=True)
 df_path1['Start_time'] = df_path1['Start_time'].apply(lambda x: x + datetime.timedelta(hours=12))
 df_path1['Start_time'] = df_path1['Start_time'].apply(lambda x: x.strftime('%H:%M'))
-print df_path1
+# print df_path1
 df_device_new = df_device_new[df_device_new['ts'].isin(df_path1['Start_time'])]
 df_device_new.reset_index(drop=True, inplace=True)
 for i in range(len(df_device_new)):
-	ts = df_device_new.loc[i, 'ts']
-	slic = df_path1[df_path1['Start_time'] == ts]
-	x = slic.loc[slic.index[0], 'X']
-	y = slic.loc[slic.index[0], 'Y']
+    ts = df_device_new.loc[i, 'ts']
+    slic = df_path1[df_path1['Start_time'] == ts]
+    x = slic.loc[slic.index[0], 'X']
+    y = slic.loc[slic.index[0], 'Y']
+    df_device_new.loc[i, 'act_loc'] = [x, y]
 
-	df_device_new.loc[i, 'act_loc'] = [x, y]
-
-print df_device_new	
+print(df_device_new)
 df_device_new.to_csv('moto_fing_pred.csv')
