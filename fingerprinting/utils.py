a94cf725d1847c33a51b514ffba7659a05ca666c
Harish Thuwal <hthuwal@gmail.com>
2018-03-20 22:33:05 +0530

    Add function for rmsq

--- fingerprinting/utils.py
+++ fingerprinting/utils.py
@@ -1,6 +1,17 @@
 import numpy as np
+import math
 
 def euclidean_distance(a, b):
 	a = np.array(a)
 	b = np.array(b)
 	return np.linalg.norm(a-b)
+
+def root_mean_square_error(validation, test):
+    error = []
+    for time in validation:
+        if time in test:
+            error.append(euclidean_distance(validation[time], test[time]))
+
+    error = [e * e for e in error]
+    l = len(error)
+    return math.sqrt(sum(error) / l)
