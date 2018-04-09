741fe855c06e92bfd3631fad1f49712968d9bebe
sansiddhjain <sansiddh@gmail.com>
2018-04-10 01:43:23 +0530

    HMM updated to work on Panini1

--- unsupervised_hmm/hmmlearn_imp.py
+++ unsupervised_hmm/hmmlearn_imp.py
@@ -2,18 +2,21 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+import timeit
 
 from hmmlearn import hmm
 from sklearn.preprocessing import LabelEncoder
+from sklearn.externals import joblib
 
+start = timeit.default_timer()
 np.random.seed(42)
 
-df = pd.read_csv('modified_training_day_log.csv')
+cid = sys.argv[1]
+
+df = pd.read_csv('modified_log_controller_'+cid+'.csv')
 del df['Unnamed: 0']
 
-df = df[df['controllerid'] == 1]
 df.reset_index(inplace=True, drop=True)
-df['pwr'] = df['pwr'].astype('int64')
 
 grp = df.groupby(['mac'])
 df1 = grp.count()
@@ -30,7 +33,7 @@ grp = df.groupby(['mac'])
 lst = list(grp)
 observed_sequences = []
 observed_timestamps = []
-len_samples = np.zeros(len(lst))
+len_samples = np.zeros(len(lst), dtype=int)
 for i in range(len(lst)):
     x = lst[i]
     ts_list = np.asarray(x[1]['ts'])
@@ -45,7 +48,9 @@ X = np.asarray(df['pwr'])
 le = LabelEncoder()
 le.fit(X)
 print(le.classes_)
+print(le.transform(X))
 X_d = le.transform(X)
+print(le.inverse_transform(X_d))
 # X_d = np.asarray(list(map(int, X_d)))
 X_d = X_d.reshape((len(X_d), 1))
 
@@ -58,6 +63,29 @@ for x in range(-46, 31): #XRange
 		count += 1
 n_hidden_states = len(hidden_states)
 
-model = hmm.MultinomialHMM(n_components=n_hidden_states).fit(np.atleast_2d(X_d), len_samples)
+# model = hmm.MultinomialHMM(n_components=n_hidden_states).fit(np.atleast_2d(X_d).T, len_samples)
+print(len_samples)
+print('Starting Training...')
+model = hmm.MultinomialHMM(n_components=n_hidden_states).fit(X_d, len_samples)
+print('Finished Training!')
 model.monitor_
 model.monitor_.converged
+filename = 'model_'+cid+'.pkl'
+joblib.dump(model, filename)
+stop = timeit.default_timer()
+print(stop-start)
+
+macs = ["00:0c:e7:4f:38:a5", "84:38:38:f6:58:40", "c0:ee:fb:72:0c:27", "18:dc:56:8c:27:56", "80:58:f8:d8:ad:e1"]
+names = ["Micromax", "Samsung S5", "oneplus x", "Yureka", "Moto"]
+
+df_test = pd.read_csv('modified_training_day_logs.csv')
+for i in range(len(macs)):
+    df1 = df_test[(df_test['controllerid'] == 1) & (df_test['mac'] == macs[i])]
+    df1.reset_index(inplace=True, drop=True)
+    X = np.asarray(df1['pwr'])
+    X_d = le.transform(X)
+    X_d = X_d.reshape((len(X_d), 1))
+    hidden_states = model.predict(X_d)
+    thefile = open('hidden_states_model_'+cid+'_device_'+names[i]+'.txt', 'w')
+    thefile.write(list(map(hidden_states)))
+    thefile.close()
---
+++ unsupervised_hmm/pre_format_data_panini.py
@@ -0,0 +1,43 @@
+import pandas as pd 
+import numpy as np 
+import os
+import sys
+
+nas_dict = {1 : '2946', 2 : '3596', 3 : '3597', 4 : '3598'}
+cid = sys.argv[1]
+nas = nas_dict[int(cid)]
+
+f = os.listdir('../spencers_data/')
+print(f)
+n = [j for j in range(1, 16)]
+days = ['2018-1-'+str(j) for j in n]
+print(days)
+f = [j for j in f if j in days]
+print(f)
+tmp = os.listdir('../spencers_data/2018-1-1/NAS_'+nas+'/')
+df = pd.read_json('../spencers_data/2018-1-1/NAS_'+nas+'/'+tmp[0], lines=True)
+df['ts'] = pd.to_datetime(df['ts'], infer_datetime_format=True)
+for day in f:
+	hours = os.listdir('../spencers_data/'+day+'/NAS_'+nas+'/')
+	print(hours)
+	hours = [j for j in hours if '.log' in j]
+	print(hours)
+	for log_file in hours:
+		df_hour = pd.read_json('../spencers_data/'+day+'/NAS_'+nas+'/'+log_file, lines=True)
+		df_hour['ts'] = pd.to_datetime(df_hour['ts'], infer_datetime_format=True)
+		df = df.append(df_hour, ignore_index=True)
+		print(str(log_file)+' for day '+str(day)+' appended.')
+df.drop_duplicates(inplace=True)
+df.reset_index(drop=True, inplace=True)
+df['ts']= df['ts'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
+df['ts'] = pd.to_datetime(df['ts'], infer_datetime_format=True)
+df_ts1s = df.groupby(['nasid', 'controllerid', 'position', 'ts', 'mac'])
+df = df_ts1s.mean()
+df.reset_index(inplace=True)
+df['pwr'] = df['pwr'].astype('int64')
+print('df formatted.')
+del df['nasid']
+del df['position']
+print(df)
+
+df.to_csv('modified_log_controller_'+cid+'.csv')
