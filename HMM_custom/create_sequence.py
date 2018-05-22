71cd294217f9cb25aa94b7a3a546c6ec20405b10
Harish Thuwal <hthuwal@gmail.com>
2018-05-22 21:36:01 +0530

    Add Doc Strings to give_seq function

--- HMM_custom/create_sequence.py
+++ HMM_custom/create_sequence.py
@@ -12,6 +12,19 @@ nas2ap = {
 
 
 def give_seq(directory, num_obs_per_seq):
+    """
+    Extract observation sequences from the log files.
+
+    Where each observation is a dictionary with key = accesspoint, value = power
+
+    Arguments:
+        directory -- path to directory where log file is present
+        num_obs_per_seq -- length of observation sequences
+
+    Returns:
+        ls_of_obs_seq -- List of observation sequences
+
+    """
     data = None
     for nas in nas2ap:
         folder_path = os.path.join(directory, "NAS_%d" % (nas))
@@ -64,4 +77,4 @@ def give_seq(directory, num_obs_per_seq):
 
 
 if __name__ == '__main__':
-    hc = give_seq("spencers/2018-1-1", 50)
+    hc = give_seq("../spencers/2018-1-1", 50)
