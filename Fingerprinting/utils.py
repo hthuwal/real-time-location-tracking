31fc9e954635e439df9cd71e9b420708a7d12614
Harish Thuwal <hthuwal@gmail.com>
2018-05-22 21:24:31 +0530

    Pep8 and docstrings

--- Fingerprinting/utils.py
+++ Fingerprinting/utils.py
@@ -1,17 +1,21 @@
 import numpy as np
 import math
 
+
 def euclidean_distance(a, b):
-	a = np.array(a)
-	b = np.array(b)
-	return np.linalg.norm(a-b)
+    """Calculate the euclidean distance between vectors a and b."""
+    a = np.array(a)
+    b = np.array(b)
+    return np.linalg.norm(a - b)
+
 
 def root_mean_square_error(validation, test):
+    """RMSQ error."""
     error = []
     for time in validation:
         if time in test:
             error.append(euclidean_distance(validation[time], test[time]))
 
     error = [e * e for e in error]
-    l = len(error)
-    return math.sqrt(sum(error) / l)
+    length = len(error)
+    return math.sqrt(sum(error) / length)
